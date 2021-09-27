a6c739bb4f4d659199d3e62e88aa02be03164c41
Xuan Tang <txstc55@gmail.com>
2021-09-27 22:50:14 +0800

    add download

--- client/src/views/Gif.vue
+++ client/src/views/Gif.vue
@@ -11,17 +11,17 @@
             </div>
           </v-col>
           <v-col cols="2" md="4" sm="6" xs="6">
-            <v-row class="mt-3"
+            <v-row class="pt-2"
               ><v-btn
                 block
                 width="100%"
                 class="output-button"
                 color="rgb(10, 192, 144)"
-                @click="getNewGif"
+                @click="getNewImage"
                 >ANOTHER ONE</v-btn
               ></v-row
             >
-            <v-row class="mt-8"
+            <v-row class="pt-2"
               ><v-btn
                 block
                 width="100%"
@@ -30,6 +30,16 @@
                 @click="requestExplaination"
                 :disabled="!canRequestExplaination"
                 >{{ requestExplainationText }}</v-btn
+              ></v-row
+            >
+            <v-row class="pt-2"
+              ><v-btn
+                block
+                width="100%"
+                class="output-button"
+                color="rgb(10, 192, 144)"
+                @click="download"
+                >SAVE IT</v-btn
               ></v-row
             >
           </v-col>
@@ -82,6 +92,21 @@ export default {
           this.requestExplainationText = "REQUEST ACKNOWLEDGED";
           this.canRequestExplaination = false;
         }
+      });
+    },
+    download() {
+      axios({
+        url: host + "image/content/" + this.imgID,
+        method: "GET",
+        responseType: "blob",
+      }).then((response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", this.imgID + ".gif");
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
       });
     },
   },
--- client/src/views/Video.vue
+++ client/src/views/Video.vue
@@ -13,17 +13,17 @@
             </div>
           </v-col>
           <v-col cols="2" md="4" sm="6" xs="6">
-            <v-row class="mt-3"
+            <v-row class="pt-2"
               ><v-btn
                 block
                 width="100%"
                 class="output-button"
                 color="rgb(10, 192, 144)"
-                @click="getNewVideo"
+                @click="getNewImage"
                 >ANOTHER ONE</v-btn
               ></v-row
             >
-            <v-row class="mt-8"
+            <v-row class="pt-2"
               ><v-btn
                 block
                 width="100%"
@@ -32,6 +32,16 @@
                 @click="requestExplaination"
                 :disabled="!canRequestExplaination"
                 >{{ requestExplainationText }}</v-btn
+              ></v-row
+            >
+            <v-row class="pt-2"
+              ><v-btn
+                block
+                width="100%"
+                class="output-button"
+                color="rgb(10, 192, 144)"
+                @click="download"
+                >SAVE IT</v-btn
               ></v-row
             >
           </v-col>
@@ -86,6 +96,21 @@ export default {
           this.requestExplainationText = "REQUEST ACKNOWLEDGED";
           this.canRequestExplaination = false;
         }
+      });
+    },
+        download() {
+      axios({
+        url: host + "image/content/" + this.imgID,
+        method: "GET",
+        responseType: "blob",
+      }).then((response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", this.imgID + ".mp4");
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
       });
     },
   },
