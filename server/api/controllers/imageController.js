5034d6392690065362c691d40b4ba4c416fff555
Xuan Tang <txstc55@gmail.com>
2021-10-03 21:09:18 +0800

    refactorize I guess

---
+++ server/api/controllers/imageController.js
@@ -0,0 +1,170 @@
+const mongoose = require("mongoose");
+const images = mongoose.model("images");
+const fs = require("fs");
+// For todays date;
+Date.prototype.today = function () {
+  return (
+    (this.getDate() < 10 ? "0" : "") +
+    this.getDate() +
+    "/" +
+    (this.getMonth() + 1 < 10 ? "0" : "") +
+    (this.getMonth() + 1) +
+    "/" +
+    this.getFullYear()
+  );
+};
+
+// For the time now
+Date.prototype.timeNow = function () {
+  return (
+    (this.getHours() < 10 ? "0" : "") +
+    this.getHours() +
+    ":" +
+    (this.getMinutes() < 10 ? "0" : "") +
+    this.getMinutes() +
+    ":" +
+    (this.getSeconds() < 10 ? "0" : "") +
+    this.getSeconds()
+  );
+};
+
+exports.pick_one_image = async (_, res) => {
+  return await images.count().exec(async (err1, count) => {
+    if (err1) res.send(err1);
+    var random = Math.floor(Math.random() * count);
+    return await images
+      .findOne()
+      .skip(random)
+      .exec(function (err2, result) {
+        if (err2) res.send(err2);
+        var datetime =
+          "Query at: " + new Date().today() + " @ " + new Date().timeNow();
+        console.log("Image ", result._id, result.path, datetime);
+        res.send({
+          id: result._id,
+          explaination: result.explaination,
+          request_explaination: result.request_explaination,
+        });
+        res.end();
+        res.connection.end();
+      });
+  });
+};
+
+exports.pick_id_image = async (req, res) => {
+  return await images.findById(req.params.id).then(async (result) => {
+    if (!result) {
+      res.status(404).send({
+        message: "FAIL TO GET IMAGE INFO: " + req.params.id,
+      });
+    } else {
+      var datetime =
+        "Query at: " + new Date().today() + " @ " + new Date().timeNow();
+      console.log("Image with ID ", result._id, result.path, datetime);
+      res.send({
+        id: result._id,
+        explaination: result.explaination,
+        request_explaination: result.request_explaination,
+      });
+      res.end();
+      res.connection.end();
+    }
+  });
+};
+
+exports.get_image_by_id = async (req, res) => {
+  return await images.findById(req.params.id).then(async (result) => {
+    if (!result) {
+      res.status(404).send({
+        message: "FAIL TO GET IMAGE " + id,
+      });
+    } else {
+      const imgdata = fs.readFileSync(result.path);
+      const imgExtension = result.path.split(".").at(-1);
+      res.contentType = "image/png";
+      res.setHeader(
+        "Content-Disposition",
+        'attachment; filename="' + result._id + "." + imgExtension + '"'
+      );
+      res.send(imgdata);
+      res.end();
+      res.connection.end();
+    }
+  });
+};
+
+exports.get_image_explaination_by_id = async (req, res) => {
+  return await images.findById(req.params.id).then(async (result) => {
+    if (!result) {
+      res.status(404).send({
+        message: "FAIL TO GET IMAGE " + id,
+      });
+    } else {
+      const explaination = result.explaination;
+      res.send({ explaination: explaination });
+      res.end();
+      res.connection.end();
+    }
+  });
+};
+
+exports.get_image_request_by_id = async (req, res) => {
+  return await images.findById(req.params.id).then(async (result) => {
+    if (!result) {
+      res.status(404).send({
+        message: "FAIL TO GET IMAGE " + id,
+      });
+    } else {
+      const request_explaination = result.request_explaination;
+      res.send({ request_explaination: request_explaination });
+      res.end();
+      res.connection.end();
+    }
+  });
+};
+
+exports.post_image_request_by_id = async (req, res) => {
+  const id = parseInt(req.params.id);
+  images.findByIdAndUpdate(
+    id,
+    { request_explaination: true },
+    (err, result) => {
+      if (err) {
+        console.log(
+          "Image request explaination error, id: ",
+          id,
+          ", err: ",
+          err
+        );
+        res.send({ success: false });
+      } else {
+        var datetime =
+          "Query at: " + new Date().today() + " @ " + new Date().timeNow();
+        console.log("Image request explaination for id: ", id, datetime);
+        res.send({ success: true });
+      }
+    }
+  );
+};
+
+exports.post_image_explaination_by_id = async (req, res) => {
+  const id = parseInt(req.params.id);
+  images.findByIdAndUpdate(
+    id,
+    { request_explaination: false, explaination: req.body.text },
+    (err, result) => {
+      if (err) {
+        console.log(
+          "Image adding explaination error, id: ",
+          id,
+          ", err: ",
+          err
+        );
+        res.send({ success: false });
+      } else {
+        console.log("Image explaination added for id: ", id);
+        res.send({ success: true });
+      }
+    }
+  );
+};
--- server/api/routes/route.js
+++ server/api/routes/route.js
@@ -1,44 +1,48 @@
-const controllers = require("../controllers/controller");
+const imageControllers = require("../controllers/imageController");
+const gifControllers = require("../controllers/gifController");
+const videoControllers = require("../controllers/videoController");
 
 module.exports = (app) => {
-    app.route("/image").get(controllers.pick_one_image); // get a random image id
-    app.route("/image/:id").get(controllers.pick_id_image); // get a random image id
+    app.route("/image").get(imageControllers.pick_one_image); // get a random image id
+    app.route("/image/:id").get(imageControllers.pick_id_image); // get a specific image with id
 
-    app.route("/image/content/:id").get(controllers.get_image_by_id); // get the content of image by id
+    app.route("/image/content/:id").get(imageControllers.get_image_by_id); // get the content of image by id
 
     app.route("/image/explaination/:id")
-        .get(controllers.get_image_explaination_by_id) // get the image explaination by id
-        .post(controllers.post_image_explaination_by_id); // post image explaination by id
+        .get(imageControllers.get_image_explaination_by_id) // get the image explaination by id
+        .post(imageControllers.post_image_explaination_by_id); // post image explaination by id
 
     app.route("/image/request/:id")
-        .get(controllers.get_image_request_by_id) // get the image explaination request by id
-        .post(controllers.post_image_request_by_id); // post the image explaination request by id
+        .get(imageControllers.get_image_request_by_id) // get the image explaination request by id
+        .post(imageControllers.post_image_request_by_id); // post the image explaination request by id
 
 
-    app.route("/gif").get(controllers.pick_one_gif); 
+    app.route("/gif").get(gifControllers.pick_one_gif); 
+    app.route("/gif/:id").get(gifControllers.pick_id_gif); // get a specific gif with id
 
-    app.route("/gif/content/:id").get(controllers.get_gif_by_id); 
+    app.route("/gif/content/:id").get(gifControllers.get_gif_by_id); 
 
     app.route("/gif/explaination/:id")
-        .get(controllers.get_gif_explaination_by_id) 
-        .post(controllers.post_gif_explaination_by_id); 
+        .get(gifControllers.get_gif_explaination_by_id) 
+        .post(gifControllers.post_gif_explaination_by_id); 
 
     app.route("/gif/request/:id")
-        .get(controllers.get_gif_request_by_id) 
-        .post(controllers.post_gif_request_by_id); 
+        .get(gifControllers.get_gif_request_by_id) 
+        .post(gifControllers.post_gif_request_by_id); 
 
 
-    app.route("/video").get(controllers.pick_one_video); 
+    app.route("/video").get(videoControllers.pick_one_video); 
+    app.route("/video/:id").get(videoControllers.pick_id_video); // get a specific video with id
 
-    app.route("/video/content/:id").get(controllers.get_video_by_id); 
+    app.route("/video/content/:id").get(videoControllers.get_video_by_id); 
 
     app.route("/video/explaination/:id")
-        .get(controllers.get_video_explaination_by_id) 
-        .post(controllers.post_video_explaination_by_id); 
+        .get(videoControllers.get_video_explaination_by_id) 
+        .post(videoControllers.post_video_explaination_by_id); 
 
     app.route("/video/request/:id")
-        .get(controllers.get_video_request_by_id) 
-        .post(controllers.post_video_request_by_id); 
+        .get(videoControllers.get_video_request_by_id) 
+        .post(videoControllers.post_video_request_by_id); 
 };
 
 // test on 46479771
